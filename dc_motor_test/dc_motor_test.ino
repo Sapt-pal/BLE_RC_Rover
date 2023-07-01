18e40c6e3b0c804e70557663718ee6d3c779f9f1
Sapt-pal <saptarshipal04@gmail.com>
2023-07-01 14:09:05 +0530

    updating dc_motor_test.ino

--- dc_motor_test/dc_motor_test.ino
+++ dc_motor_test/dc_motor_test.ino
@@ -6,21 +6,21 @@
 #include <AFMotor.h>
 
 AF_DCMotor motor1(1);
-//AF_DCMotor motor2(2);
-//AF_DCMotor motor3(3);
-//AF_DCMotor motor4(4);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
 
 void setup() {
 
   motor1.setSpeed(255);
-  //motor2.setSpeed(255);
-  //motor3.setSpeed(255);
-  //motor4.setSpeed(255);
+  motor2.setSpeed(255);
+  motor3.setSpeed(255);
+  motor4.setSpeed(255);
   
   motor1.run(RELEASE);
-  //motor2.run(RELEASE);
-  //motor3.run(RELEASE);
-  //motor4.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
   
 }
 
@@ -28,27 +28,27 @@ void loop() {
   
   
   motor1.run(FORWARD);
-  //motor2.run(FORWARD);
-  //motor3.run(FORWARD);
-  //motor4.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
 
   delay(4000);
   motor1.run(RELEASE);
-  //motor2.run(RELEASE);
-  //motor3.run(RELEASE);
-  //motor4.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
   delay(200);
   
   motor1.run(BACKWARD);
-  //motor2.run(BACKWARD);
-  //motor3.run(BACKWARD);
-  //motor4.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
 
   delay(4000);
   motor1.run(RELEASE);
-  //motor2.run(RELEASE);
-  //motor3.run(RELEASE);
-  //motor4.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
   delay(200);
  
  
