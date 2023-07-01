853f5f6a98ccd20466130690ed78b45718723195
Sapt-pal <saptarshipal04@gmail.com>
2023-07-01 13:50:27 +0530

    Added motor control, pending debugging

--- ble_blink/ble_blink.ino
+++ ble_blink/ble_blink.ino
@@ -1,10 +1,27 @@
+#include <AFMotor.h>
+                          //_________
+AF_DCMotor motor1(1);     //| 1   2 |
+AF_DCMotor motor2(2);     //|       |
+AF_DCMotor motor3(3);     //|       |
+AF_DCMotor motor4(4);     //| 3   4 |
+                          //¯¯¯¯¯¯¯¯¯
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
+  //pinMode(A1, OUTPUT);
+  //pinMode(A2, OUTPUT);
+  //pinMode(A3, OUTPUT);
+  //pinMode(A4, OUTPUT);
+  motor1.setSpeed(255);
+  motor2.setSpeed(255);
+  motor3.setSpeed(255);
+  motor4.setSpeed(255);
+  
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
 }
 
 void loop() {
@@ -12,23 +29,36 @@ void loop() {
   if (Serial.available()) {
     char data = Serial.read();
     switch (data) {
-      case '0': 
-        analogWrite(A0, 200); 
+      case 'F': 
+        //analogWrite(A0, 200);
+        motor1.run(FORWARD);
+        motor2.run(FORWARD);
+        motor3.run(FORWARD);
+        motor4.run(FORWARD); 
         break;
-      case '1': 
-        analogWrite(A1, 200);
+      case 'B': 
+        //analogWrite(A1, 200);
+        motor1.run(BACKWARD);
+        motor2.run(BACKWARD);
+        motor3.run(BACKWARD);
+        motor4.run(BACKWARD);
         break;
-      case '2': 
-        analogWrite(A2, 200);
+      case 'L': 
+        //analogWrite(A2, 200);
+        motor1.run(FORWARD);
+        motor3.run(FORWARD);
         break;
-      case '3': 
-        analogWrite(A3, 200);
+      case 'R': 
+        //analogWrite(A3, 200);
+        motor2.run(FORWARD);
+        motor4.run(FORWARD);
+        break;
+      case 'H': 
+        analogWrite(A0, 200);
         break;
       default: 
         break;
     }
     Serial.println(data);
   }
-
-
 }
